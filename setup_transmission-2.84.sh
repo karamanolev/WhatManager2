0d291f3ffeec9ada0288dc6f05fdf1821851c800
Ivailo Karamanolev <ivailo@karamanolev.com>
2014-09-03 22:07:52 -0700

    Try to make libevent/transmission install.

--- setup_transmission-2.84.sh
+++ setup_transmission-2.84.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -e
 sudo apt-get install build-essential libcurl4-openssl-dev intltool checkinstall
 
-wget https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
-tar xvzf libevent-2.0.21-stable.tar.gz
+tar xvzf deps/libevent-2.0.21-stable.tar.gz
 cd libevent-2.0.21-stable
 CFLAGS="-Os -march=native" ./configure && make -j2
 sudo make install
 cd ..
 
-wget https://transmission.cachefly.net/transmission-2.84.tar.xz
-tar xvJf transmission-2.84.tar.xz
+tar xvJf deps/transmission-2.84.tar.xz
 cd transmission-2.84
 CFLAGS="-Os -march=native" ./configure && make -j2
-sudo checkinstall
+sudo checkinstall -y
 cd ..
