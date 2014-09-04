e8313465c87f2bbb28dc229fb52392a86c9b6629
Ivailo Karamanolev <ivailo@karamanolev.com>
2014-09-03 23:22:08 -0700

    Add a separate setup file for Travis.

---
+++ ci_setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo apt-get install libapache2-mod-wsgi python-pip flac lame sox mktorrent libmysqlclient-dev python-dev libxml2-dev libxslt1-dev curl
+pip install -r requirements.txt
+sudo chmod 777 media
