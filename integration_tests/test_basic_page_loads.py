efa3fd01e5cfde5895463cc685d5b8f26a886c7f
Ivailo Karamanolev <ivailo@karamanolev.com>
2014-09-04 20:32:09 -0700

    Add more basic page load tests.

--- integration_tests/test_basic_page_loads.py
+++ integration_tests/test_basic_page_loads.py
@@ -14,6 +14,12 @@ class TestBasicPageLoads(testcases.TestCase):
 
         self.client = Client()
 
+    def login(self):
+        response = self.client.post('/user/login',
+                                    {'username': 'john_doe', 'password': 'password'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, 'http://testserver/')
+
     def test_require_login(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 302)
@@ -33,8 +39,83 @@ class TestBasicPageLoads(testcases.TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, 'http://testserver/dummy_url')
 
+    def test_book_uploads(self):
+        self.login()
+        response = self.client.post('/books/uploads')
+        self.assertEqual(response.status_code, 200)
+
+    def test_torrents(self):
+        self.login()
+        response = self.client.post('/torrents')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_log(self):
+        self.login()
+        response = self.client.post('/view_log')
+        self.assertEqual(response.status_code, 200)
+
+    def test_checks(self):
+        self.login()
+        response = self.client.post('/checks')
+        self.assertEqual(response.status_code, 200)
+
+    def test_torrents(self):
+        self.login()
+        response = self.client.post('/torrents')
+        self.assertEqual(response.status_code, 200)
+
     def test_profile(self):
-        self.client.post('/user/login',
-                         {'username': 'john_doe', 'password': 'password'})
+        self.login()
         response = self.client.get('/profile/')
         self.assertEqual(response.status_code, 200)
+
+    def test_queue(self):
+        self.login()
+        response = self.client.get('/queue/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_stats(self):
+        self.login()
+        response = self.client.get('/stats')
+        self.assertEqual(response.status_code, 200)
+
+    def test_userscripts(self):
+        self.login()
+        response = self.client.get('/userscripts')
+        self.assertEqual(response.status_code, 200)
+
+    def test_part_torrent_stats(self):
+        self.login()
+        response = self.client.get('/part/torrent_stats')
+        self.assertEqual(response.status_code, 200)
+
+    def test_part_recent_log(self):
+        self.login()
+        response = self.client.post('/part/recent_log',
+                                    {'log_types': 'error,action,info', 'count': 10})
+        self.assertEqual(response.status_code, 200)
+
+    def test_part_error_torrents(self):
+        self.login()
+        response = self.client.get('/part/error_torrents')
+        self.assertEqual(response.status_code, 200)
+
+    def test_part_downloading(self):
+        self.login()
+        response = self.client.get('/part/downloading')
+        self.assertEqual(response.status_code, 200)
+
+    def test_part_recently_downloaded(self):
+        self.login()
+        response = self.client.get('/part/recently_downloaded')
+        self.assertEqual(response.status_code, 200)
+
+    def test_part_queue_stats(self):
+        self.login()
+        response = self.client.get('/queue/part/queue_stats')
+        self.assertEqual(response.status_code, 200)
+
+
+
+
+
