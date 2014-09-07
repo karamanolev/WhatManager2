454a2a4df4b2ee0f6998ebfb320fe1c69768b209
Ivailo Karamanolev <ivailo@karamanolev.com>
2014-09-07 13:26:34 -0700

    Refactor WhatTorrent refreshing in utils and add management command.

---
+++ WhatManager2/management/commands/what_data_updater.py
@@ -0,0 +1,20 @@
+import time
+
+from django.core.management.base import BaseCommand
+
+from home.models import get_what_client
+import what_json.utils
+
+
+class Command(BaseCommand):
+    help = 'Fetches your oldest data from What.CD again to refresh information.'
+
+    def handle(self, *args, **options):
+        what_client = get_what_client(lambda: None)
+        while True:
+            response = what_json.utils.refresh_whattorrent(what_client)
+            print response
+            if response['success']:
+                time.sleep(5)
+            else:
+                time.sleep(15)
---
+++ what_json/utils.py
@@ -0,0 +1,48 @@
+import json
+
+from django.utils import timezone
+
+from home.models import RequestException, TransTorrent, ReplicaSet, WhatTorrent
+
+
+def refresh_whattorrent(what_client, what_torrent=None):
+    if what_torrent is None:
+        what_torrent = WhatTorrent.objects.defer('torrent_file').order_by('retrieved')[0]
+
+    try:
+        response = what_client.request('torrent', id=what_torrent.id)['response']
+    except RequestException as ex:
+        if ex.response and type(ex.response) is dict and ex.response.get(
+                'error') == 'bad id parameter':
+            try:
+                TransTorrent.objects.get(
+                    instance__in=ReplicaSet.get_what_master().transinstance_set.all(),
+                    what_torrent=what_torrent)
+                return {
+                    'success': False,
+                    'id': what_torrent.id,
+                    'status': 'missing',
+                }
+            except TransTorrent.DoesNotExist:
+                what_torrent.delete()
+                return {
+                    'success': True,
+                    'id': what_torrent.id,
+                    'status': 'deleted',
+                }
+        else:
+            return {
+                'success': False,
+                'status': 'unknown request exception',
+            }
+
+    old_retrieved = what_torrent.retrieved
+    what_torrent.info = json.dumps(response)
+    what_torrent.retrieved = timezone.now()
+    what_torrent.save()
+    return {
+        'success': True,
+        'id': what_torrent.id,
+        'status': 'refreshed',
+        'retrieved': unicode(old_retrieved),
+    }
