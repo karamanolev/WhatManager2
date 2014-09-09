b81c682c652f7097571f89b6bd8acdbbb2ca8b54
Ivailo Karamanolev <ivailo@karamanolev.com>
2014-09-08 23:14:14 -0700

    Add get_torrent_group endpoint for what_meta.

--- what_meta/urls.py
+++ what_meta/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     '',
-    url(r'^search/torrent_groups/(.*)$', 'what_meta.views.search_torrent_groups'),
+    url(r'^torrent_groups/search/(.+)$', 'what_meta.views.search_torrent_groups'),
+    url(r'^torrent_groups/(\d+)$', 'what_meta.views.get_torrent_group'),
 )
--- what_meta/views.py
+++ what_meta/views.py
@@ -1,12 +1,41 @@
+import json
+
 from django.core import serializers
+
 from django.http.response import HttpResponse
 
+from home.models import WhatTorrent
+from player.player_utils import get_playlist_files, get_metadata_dict
 from what_meta.models import WhatTorrentGroup
 
 
 def search_torrent_groups(request, query):
     response = HttpResponse(
         serializers.serialize('json', WhatTorrentGroup.objects.filter(name__icontains=query)),
+        content_type='text/json',
+    )
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
+
+
+def get_torrent_group(request, group_id):
+    torrent_group = WhatTorrentGroup.objects.get(id=group_id)
+    torrent = WhatTorrent.objects.filter(torrent_group=torrent_group)[0]
+    data = {
+        'id': torrent_group.id,
+        'name': torrent_group.name,
+        'wikiImage': torrent_group.wiki_image,
+        'playlist': [
+            {
+                'id': 'what/' + str(torrent.id) + '#' + str(i),
+                'url': path,
+                'metadata': get_metadata_dict(path)
+            }
+            for i, path in enumerate(get_playlist_files('what/' + str(torrent.id))[1])
+        ]
+    }
+    response = HttpResponse(
+        json.dumps(data, indent=4),
         content_type='text/json',
     )
     response['Access-Control-Allow-Origin'] = '*'
