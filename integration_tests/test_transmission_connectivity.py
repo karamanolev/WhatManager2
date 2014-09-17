a5fd8ba06ed63b726f2fd1da3e535af9848fe413
Ivailo Karamanolev <ivailo@karamanolev.com>
2014-09-16 23:49:17 -0700

    Fix transmission connectivity tests.

--- integration_tests/test_transmission_connectivity.py
+++ integration_tests/test_transmission_connectivity.py
@@ -5,6 +5,5 @@ from home.models import TransInstance
 class TestTransmissionConnectivity(testcases.TestCase):
     def test_instances_connectible(self):
         for instance in TransInstance.objects.all():
-            instance = TransInstance()
             session_stats = instance.client.session_stats()
             self.assertTrue(type(session_stats) is int)
