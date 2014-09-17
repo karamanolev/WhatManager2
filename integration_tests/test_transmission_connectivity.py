df48fc2ff96f39b419f9ad92bd31cb7e80ac479c
Ivailo Karamanolev <ivailo@karamanolev.com>
2014-09-16 23:41:48 -0700

    Add bibliotik instance, check transmission connectivity tests.

---
+++ integration_tests/test_transmission_connectivity.py
@@ -0,0 +1,10 @@
+from django.test import testcases
+from home.models import TransInstance
+
+
+class TestTransmissionConnectivity(testcases.TestCase):
+    def test_instances_connectible(self):
+        for instance in TransInstance.objects.all():
+            instance = TransInstance()
+            session_stats = instance.client.session_stats()
+            self.assertTrue(type(session_stats) is int)
