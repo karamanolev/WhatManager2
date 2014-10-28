12f510aa5b098723a2e3af79c660fa308a942396
davols <david@davido.me>
2014-10-28 06:24:01 +0100

    dynamic link

--- static/whatify/player/player.js
+++ static/whatify/player/player.js
@@ -164,9 +164,7 @@ angular.
             whatPlayer.load(s.currentItem.url);
             console.log('HERE WE GOT');
             whatPlayer.play();
-                            url = window.location.href.replace(window.location.hash,
-                        '#/torrentGroups/' + torrentGroup.id) + '/play';
-            $.post( "/lastfm/scrobble", JSON.stringify({ "artist": s.currentItem.metadata.artist, "title" : s.currentItem.metadata.title, "album" : s.currentItem.metadata.album }) );
+            $.post( window.location.href.replace(window.location.hash, '').replace('whatify','lastfm/scrobble'), JSON.stringify({ "artist": s.currentItem.metadata.artist, "title" : s.currentItem.metadata.title, "album" : s.currentItem.metadata.album }) );
         };
         s.add = function(item) {
             s.items.push(item);
