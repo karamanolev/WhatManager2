da7322f7b8d7d07a5a79adc4caa774891b7fb825
Ivailo Karamanolev <ivailo@hyperscience.com>
2015-05-18 15:32:41 +0300

    Allow NULL cover_url

--- myanonamouse/models.py
+++ myanonamouse/models.py
@@ -36,7 +36,7 @@ class MAMTorrent(models.Model):
     subcategory = models.CharField(max_length=64)
     language = models.CharField(max_length=32)
     isbn = models.CharField(max_length=16)
-    cover_url = models.TextField()
+    cover_url = models.TextField(null=True)
     small_description = models.TextField()
     description = models.TextField()
     html_page = models.TextField()
