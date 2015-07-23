0fd749931f18bd33e2e150e5d8421d040edee576
david <david@davido.me>
2015-07-23 03:33:30 +0200

    Test samba4

--- ci_setup.sh
+++ ci_setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo apt-get install libapache2-mod-wsgi python-pip flac lame sox mktorrent libmysqlclient-dev python-dev libxml2-dev libxslt1-dev curl python-samba
+sudo apt-get install libapache2-mod-wsgi python-pip flac lame sox mktorrent libmysqlclient-dev python-dev libxml2-dev libxslt1-dev curl python-samba samba4
 sudo chmod 777 media
