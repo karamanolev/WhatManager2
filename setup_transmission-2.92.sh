2e21ceaf9447086ebe0bb0ae3168066c2796c95c
dangerman200k <dangerman200k@live.co.uk>
2016-07-09 22:13:38 +0100

    Add libssl-dev to transmission script, needed on Ubuntu 16.04

--- setup_transmission-2.92.sh
+++ setup_transmission-2.92.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-sudo apt-get install build-essential libcurl4-openssl-dev intltool checkinstall
+sudo apt-get install build-essential libcurl4-openssl-dev intltool checkinstall libssl-dev
 
 tar xvzf deps/libevent-2.0.22-stable.tar.gz
 cd libevent-2.0.22-stable
