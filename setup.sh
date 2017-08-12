122647b29b9f30a5a783d9eec057b59388af6367
Matt LaPaglia <lapaglia_matthew@network.lilly.com>
2017-08-12 08:51:45 -0400

    update setup.sh to work on Debian Jessie
    I needed to manually install `libjpeg8-dev` to get the setup to complete successfully.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get install -y libapache2-mod-wsgi python-pip flac lame sox mktorrent libmysqlclient-dev python-dev libxml2-dev libxslt1-dev curl
+sudo apt-get install -y libapache2-mod-wsgi python-pip flac lame sox mktorrent libmysqlclient-dev python-dev libxml2-dev libxslt1-dev curl libjpeg8-dev
 
 sudo pip install -r requirements.txt
 sudo chmod 777 media/book_data
