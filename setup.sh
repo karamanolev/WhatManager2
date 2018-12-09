437c8b7914adfebb9d523666edca67e9c3cdf1d1
MADindustries <spamayl@gmail.com>
2018-12-09 15:15:42 -0800

    Update setup.sh to use pipenv and interactively copy settings.py files for the user

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-sudo apt-get install -y libapache2-mod-wsgi python-pip flac lame sox mktorrent libmysqlclient-dev python-dev libxml2-dev libxslt1-dev curl
+sudo apt-get install -y libapache2-mod-wsgi python3-pip flac lame sox mktorrent libmysqlclient-dev python3-dev libxml2-dev libxslt1-dev curl
 
-sudo pip install -r requirements.txt
+sudo pip3 install pipenv
+pipenv install --three
+
 sudo chmod 777 media/book_data
 sudo chmod 777 media/what_image_cache
 sudo chmod 777 media/qobuz_uploads
+
+cp -i WhatManager2/settings.example.py WhatManager2/settings.py
+cp -i bibliotik/settings.example.py bibliotik/settings.py
+cp -i myanonamouse/settings.example.py myanonamouse/settings.py
+cp -i qobuz2/settings.example.py qobuz2/settings.py
