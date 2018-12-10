beada13b0732387878062f2b625ea692ec2e1069
MADindustries <spamayl@gmail.com>
2018-12-10 14:57:51 -0800

    Fixed user profile buffer and history view imports

--- what_profile/urls.py
+++ what_profile/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
 from .views import profile
+from .parts import buffer_up_down_data, profile_history
 
 app_name = 'what_profile'
 
 urlpatterns = [
     url(r'^$', profile, name='profile'),
-    url(r'^part/buffer_up_down_data$', TemplateView.as_view(
-        template_name='buffer_up_down_data.html'), name='buffer_up_down_data'),
-    url(r'^part/profile_history', TemplateView.as_view(template_name='profile_history.html'), 
-                                                       name='profile_history'),
+    url(r'^part/buffer_up_down_data$', buffer_up_down_data, name='buffer_up_down_data'),
+    url(r'^part/profile_history', profile_history, name='profile_history'),
 ]
