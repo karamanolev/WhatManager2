90be21382a6623e90f465b26aab2723154af1d16
MADindustries <spamayl@gmail.com>
2018-12-10 17:49:50 -0800

    Fixed wmqueue parts import in urls.py

--- wmqueue/urls.py
+++ wmqueue/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
 from .views import index
+from wmqueue import parts
 
 app_name = 'wmqueue'
 
 urlpatterns = [
-    url(r'^$', index, name=''),
-    url(r'^part/queue_stats$', TemplateView.as_view(template_name='queue_stats.html'), name=''),
-    url(r'^queue_pop$', TemplateView.as_view(template_name='queue_pop.html'), name=''),
-    url(r'^pop_remove', TemplateView.as_view(template_name='pop_remove.html'), name=''),
-    url(r'^do_pop$', TemplateView.as_view(template_name='do_pop.html'), name=''),
-    url(r'^auto_pop$', TemplateView.as_view(template_name='auto_pop.html'), name=''),
-    url(r'^add_artist/(.+)$', TemplateView.as_view(template_name='add_artist.html'), name=''),
-    url(r'^add_collage/(\d+)$', TemplateView.as_view(template_name='add_collage.html'), name=''),
+    url(r'^$', index, name='index'),
+    url(r'^part/queue_stats$', parts.queue_stats, name='queue_stats'),
+    url(r'^queue_pop$', parts.queue_pop, name='queue_pop'),
+    url(r'^pop_remove', parts.pop_remove, name='pop_remove'),
+    url(r'^do_pop$', parts.do_pop, name='do_pop'),
+    url(r'^auto_pop$', parts.auto_pop, name='auto_pop'),
+    url(r'^add_artist/(.+)$', parts.add_artist, name='add_artist'),
+    url(r'^add_collage/(\d+)$', parts.add_collage, name='add_collage'),
 ]
