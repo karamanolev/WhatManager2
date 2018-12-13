10d04d9964c1db4d251722ed4cd491add653bbea
MADindustries <spamayl@gmail.com>
2018-12-12 17:32:28 -0800

    Prepare to use mod_wsgi-express as primary server to simplify and avoid apache configuration

--- WhatManager2/settings.example.py
+++ WhatManager2/settings.example.py
@@ -142,7 +142,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -156,7 +156,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'static'),
+    #os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -220,6 +220,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Web Server
+    'mod_wsgi.server',
     # Library apps
     'djcelery',
     'bootstrapform',
