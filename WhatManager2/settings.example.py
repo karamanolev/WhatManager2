f30e66e3dc1852eca642a064806d25d1d499f7e8
MADindustries <spamayl@gmail.com>
2018-12-12 18:50:20 -0800

    Fix STATIC_ROOT path so mod_wsgi properly uses it

--- WhatManager2/settings.example.py
+++ WhatManager2/settings.example.py
@@ -137,7 +137,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
