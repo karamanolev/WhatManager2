6acd3f34a832e695fee110cd9ff771f5cf684f53
MADindustries <spamayl@gmail.com>
2018-12-12 18:23:07 -0800

    Add WM service during installation

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 sudo apt-get install -y python3-pip flac lame sox mktorrent curl libmysqlclient-dev
 
 sudo pip3 install pipenv
+export PIPENV_VENV_IN_PROJECT=True
 pipenv install --three
 
 sudo chmod 777 media/book_data
@@ -12,3 +13,6 @@ cp -i WhatManager2/settings.example.py WhatManager2/settings.py
 cp -i bibliotik/settings.example.py bibliotik/settings.py
 cp -i myanonamouse/settings.example.py myanonamouse/settings.py
 cp -i qobuz2/settings.example.py qobuz2/settings.py
+
+sudo pipenv run ./manage.py runmodwsgi --setup-only --port=80 --user www-data --group www-data --server-root=/etc/whatmanager-80
+sudo cp whatmanager.service /lib/systemd/system/whatmanager.service
